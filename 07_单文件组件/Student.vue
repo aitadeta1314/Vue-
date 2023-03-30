94d0a3bf03545c42effda9018ecb6f3e5081d2e8
Fenglijun <1257410909@qq.com>
2023-03-30 16:02:03 +0800

    单文件组件

---
+++ 07_单文件组件/Student.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- 组件的结构 -->
+    <div>
+        <p>学生姓名：{{ name }}</p>
+        <p>学生性别：{{ sex }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Student',
+    data() {
+        return {
+            name: 'FLJ',
+            sex: 男,
+        }
+    },
+
+}
+
+
+</script>
